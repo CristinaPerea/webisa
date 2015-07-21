edabbf410f0351d8e7147eee3534601a2ca8296a
CristinaPerea <criperrui@gmail.com>
2015-07-21 11:25:07 +0200

    Toma de valores de Json para fundings

--- leeFundingJson.php
+++ leeFundingJson.php
@@ -3,4 +3,27 @@
 	$miJson = file_get_contents($miJsonURL);
 	$miJsonDecode = json_decode($miJson);
 	//print_r($miJsonDecode);
+	foreach ($miJsonDecode as $miJsonKey => $miJsonValue) {
+		$title = $miJsonValue->{'title'};
+		$authors = "Authors: <br/> ";
+		foreach ($miJsonValue->{'authors'} as $authorsKey => $authorsValue) {
+			$authors .= $authorsValue->{'name'}." ".$authorsValue->{'surname'}.", ";
+		} 
+		$acronym = $miJsonValue->{'acronym'};
+		$organic = $miJsonValue->{'organic'};
+		$coordinator = "Coordinator: ";
+		if($miJsonValue->{'coordinator'} != null)
+			$coordinator .= $miJsonValue->{'coordinator'}->{'name'}." ".$miJsonValue->{'coordinator'}->{'surname'};
+		$startDate = $miJsonValue->{'startDate'};
+		$endDate = $miJsonValue->{'endDate'};
+
+		echo $title.'. ';
+		echo $authors.'<br/>';
+		echo $acronym;
+		echo $organic;
+		echo $coordinator;	
+		echo $startDate;
+		echo $endDate;
+
+	}
 ?>
