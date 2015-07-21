9b2143a07429d3c9df8341dd7fd0f5bf15a84ca7
CristinaPerea <criperrui@gmail.com>
2015-07-21 11:26:35 +0200

    otra vez, ahora guardado

--- leeFundingJson.php
+++ leeFundingJson.php
@@ -2,8 +2,11 @@
 	$miJsonURL = "http://www.isa.us.es/apps/api/projects";
 	$miJson = file_get_contents($miJsonURL);
 	$miJsonDecode = json_decode($miJson);
+	$arrayOnGoing = array();
+	$arrayNoOnGoing = array();
 	//print_r($miJsonDecode);
 	foreach ($miJsonDecode as $miJsonKey => $miJsonValue) {
+
 		$title = $miJsonValue->{'title'};
 		$authors = "Authors: <br/> ";
 		foreach ($miJsonValue->{'authors'} as $authorsKey => $authorsValue) {
@@ -14,16 +17,34 @@
 		$coordinator = "Coordinator: ";
 		if($miJsonValue->{'coordinator'} != null)
 			$coordinator .= $miJsonValue->{'coordinator'}->{'name'}." ".$miJsonValue->{'coordinator'}->{'surname'};
-		$startDate = $miJsonValue->{'startDate'};
-		$endDate = $miJsonValue->{'endDate'};
+		$startDate = transformaFecha($miJsonValue->{'startDate'});
+		$endDate = transformaFecha($miJsonValue->{'endDate'});
 
-		echo $title.'. ';
-		echo $authors.'<br/>';
-		echo $acronym;
-		echo $organic;
-		echo $coordinator;	
-		echo $startDate;
-		echo $endDate;
+		$miFunding = array();
+		array_push($miFunding, $title);
+		array_push($miFunding, $authors);
+		array_push($miFunding, $acronym);
+		array_push($miFunding, $organic);
+		array_push($miFunding, $coordinator);
+		array_push($miFunding, $startDate);
+		array_push($miFunding, $endDate);
+
+		if(enCurso($miJsonValue->{'endDate'})){
+			array_push($arrayOnGoing, $miFunding);
+		}else{
+			array_push($arrayNoOnGoing, $miFunding);
+		}
+
+
+		unset($miFunding);
+
+		//echo $title.'. ';
+		//echo $authors.'<br/>';
+		//echo $acronym;
+		//echo $organic;
+		//echo $coordinator;	
+		//echo $startDate;
+		//echo $endDate;
 
 	}
 ?>
