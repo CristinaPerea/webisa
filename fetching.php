f17b1aa37478352721e4b41682af9ff54594c461
CristinaPerea <criperrui@gmail.com>
2015-07-28 18:24:55 +0200

    Fetching data

--- fetching.php
+++ fetching.php
@@ -40,16 +40,37 @@
 		$bytes_escritos_membersCSV = file_put_contents($membersCSV, $miJson);
 
 		$mensaje = "";
-		$mensaje .= "Escritos 6 ficheros:<br/>";
-		$mensaje .= '<a href="publications.json">publications.json</a> con '.$bytes_escritos_publications.' bytes escritos<br/>';
-		$mensaje .= '<a href="members.json">members.json</a> con '.$bytes_escritos_members.' bytes escritos<br/>';
-		$mensaje .= '<a href="fundings.json">fundings.json</a> con '.$bytes_escritos_fundings.' bytes escritos<br/>';
-		$mensaje .= '<a href="tools.csv">tools.csv</a> con '.$bytes_escritos_tools.' bytes escritos<br/>';
-		$mensaje .= '<a href="collaborations/collaborationsCSV.csv">collaborationsCSV.csv</a> con '.$bytes_escritos_collaborations.' bytes escritos<br/>';
-		$mensaje .= '<a href="researchLines/researchLines.csv">researchLinesCSV.csv</a> con '.$bytes_escritos_researchLines.' bytes escritos<br/>';
-		$mensaje .= '<a href="members/members.csv">members.csv</a> con '.$bytes_escritos_membersCSV.' bytes escritos<br/>';
+		$mensaje .= '<h3>Data loaded:</h3>';
+		$mensaje .= '<a href="publications.json">publications.json</a>, '.$bytes_escritos_publications.' bytes escritos<br/>';
+		$mensaje .= 'From: <br/>';
+		$mensaje .= '-[[<a href="http://www.isa.us.es/apps/api/publications/">http://www.isa.us.es/apps/api/publications/</a>]] (ISA'."'".'s Drupal) <br/>';
+		$mensaje .= '<br/>';
+		$mensaje .= '<a href="members.json">members.json</a>, '.$bytes_escritos_members.' bytes escritos<br/>';
+		$mensaje .= 'From: <br/>';
+		$mensaje .= '-[[<a href="http://www.isa.us.es/apps/api/members/">http://www.isa.us.es/apps/api/members/</a>]] (ISA'."'".'s Drupal) <br/>';
+		$mensaje .= '<br/>';
+		$mensaje .= '<a href="members/members.csv">members.csv</a>, '.$bytes_escritos_membersCSV.' bytes escritos<br/>';
+		$mensaje .= 'From: <br/>';
+		$mensaje .= '-[[<a href="https://docs.google.com/spreadsheets/d/1-y5_vwyDSa3yJIdwy7O4R-l_CLK180WTsK_7gFt8EGw/pub?gid=0&single=true&output=csv">https://docs.google.com/spreadsheets/d/1-y5_vwyDSa3yJIdwy7O4R-l_CLK180WTsK_7gFt8EGw/pub?gid=0&single=true&output=csv</a>]] ("Members" sheet in Google Drive)<br/>';
+		$mensaje .= '<br/>';
+		$mensaje .= '<a href="fundings.json">fundings.json</a>, '.$bytes_escritos_fundings.' bytes escritos<br/>';
+		$mensaje .= 'From: <br/>';
+		$mensaje .= '-[[<a href="http://www.isa.us.es/apps/api/projects/">http://www.isa.us.es/apps/api/projects/</a>]] (ISA'."'".'s Drupal) <br/>';
+		$mensaje .= '<br/>';
+		$mensaje .= '<a href="tools.csv">tools.csv</a>, '.$bytes_escritos_tools.' bytes escritos<br/>';
+		$mensaje .= 'From: <br/>';
+		$mensaje .= '-[[<a href="https://docs.google.com/spreadsheets/d/1ysrUC9ls0ElV39V1IXXseWubjoQxwC4-uneFRustaKM/pub?gid=0&single=true&output=csv">https://docs.google.com/spreadsheets/d/1ysrUC9ls0ElV39V1IXXseWubjoQxwC4-uneFRustaKM/pub?gid=0&single=true&output=csv</a>]] ("Tools" sheet in Google Drive)<br/>';
+		$mensaje .= '<br/>';
+		$mensaje .= '<a href="collaborations/collaborationsCSV.csv">collaborationsCSV.csv</a>, '.$bytes_escritos_collaborations.' bytes escritos<br/>';
+		$mensaje .= 'From: <br/>';
+		$mensaje .= '-[[<a href="https://docs.google.com/spreadsheets/d/1DqBiUjIaL0nyPQ89VWP5pt3uqxrszaobeMfD_LyK3qM/pub?output=csv">https://docs.google.com/spreadsheets/d/1DqBiUjIaL0nyPQ89VWP5pt3uqxrszaobeMfD_LyK3qM/pub?output=csv</a>]] ("Collaborations" sheet in Google Drive)<br/>';
+		$mensaje .= '<br/>';
+		$mensaje .= '<a href="researchLines/researchLines.csv">researchLinesCSV.csv</a>, '.$bytes_escritos_researchLines.' bytes escritos<br/>';
+		$mensaje .= 'From: <br/>';
+		$mensaje .= '-[[<a href="https://docs.google.com/spreadsheets/d/171m6bh9V3-NfkqB3V49laxFHEYrwD28jZX36egZnTtM/pub?output=csv">https://docs.google.com/spreadsheets/d/171m6bh9V3-NfkqB3V49laxFHEYrwD28jZX36egZnTtM/pub?output=csv</a>]] ("Research Lines" sheet in Google Drive)<br/>';
+
 		if($miJson)
 			echo $mensaje;
 		else
-			echo "No se puede leer de las APIs";
+			echo "No se ha podido leer la información";
 ?>
